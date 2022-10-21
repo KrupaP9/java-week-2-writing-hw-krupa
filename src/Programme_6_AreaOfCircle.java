277b2d6044c44233620f741c0af1dbd024b1e0fc
KrupaP9 <krupanpandya@gmail.com>
2022-10-21 23:24:31 +0100

    My Writing Homework Week 7

---
+++ src/Programme_6_AreaOfCircle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Write a program to calculate the area of a triangle.
+ */
+public class Programme_6_AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Radius : ");
+        double radius = scanner.nextDouble();
+        areaOfCircle(radius);
+        // closing the scanner object
+        scanner.close();
+    }
+    //Calculating the area of circle
+    public static void areaOfCircle(double radius){
+        double pi = Math.PI;
+        double area = (pi *radius * radius);
+        System.out.println("The Area of Circle is : "+area);
+    }
+}
